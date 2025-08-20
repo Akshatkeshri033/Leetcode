f84915c0c2e2ef353b51c96f17556db45cc37bbb
Akshatkeshri033 <147469838+Akshatkeshri033@users.noreply.github.com>
2025-08-20 17:49:19 +0530

    Create 177-NthHighestSalary

---
+++ 177-NthHighestSalary/177-NthHighestSalary.sql
@@ -0,0 +1,12 @@
+-- Last updated: 8/20/2025, 5:49:20 PM
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      SELECT DISTINCT(salary) from Employee order by salary DESC
+      LIMIT 1 OFFSET N
+      
+  );
+END
+
+#pls upvote if you find solution easy to undestand....!! Thanks..!!!
