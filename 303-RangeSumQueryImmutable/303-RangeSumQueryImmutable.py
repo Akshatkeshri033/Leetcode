367d9ab50bdce4a1049f0238f24c2938dd286c5c
Akshatkeshri033 <147469838+Akshatkeshri033@users.noreply.github.com>
2026-07-04 19:02:38 +0530

    Create 303-RangeSumQueryImmutable

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.py
@@ -0,0 +1,13 @@
+# Last updated: 7/4/2026, 7:02:47 PM
+from typing import List
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = [0]
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
+        
